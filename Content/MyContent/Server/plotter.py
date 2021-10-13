69a1c9f5c6e5e835b0d10f400349fe28c8153a18
MauriVass <maurizio140333@gmail.com>
2021-10-13 12:36:49 +0200

    Added direction colors on graphs

--- Content/MyContent/Server/plotter.py
+++ Content/MyContent/Server/plotter.py
@@ -20,6 +20,13 @@ if(plot_losses is True):
 	ax.plot(x,y,linewidth=.05, marker='o', markersize=0.5)
 else:
 	for i in range(4):
+		colors = []
+		for r in rewards['IsClockwise']:
+			if(r==0):
+				c = 'r'
+			else:
+				c = 'b'
+			colors.append(c)
 		fig, axs = plt.subplots(2,figsize=(24,8), sharex=True)
 		rolling = 1 + i*2
 		rewards['rolling_avg_reward'] = rewards['totalReward'].rolling(rolling).mean()
@@ -31,10 +38,12 @@ else:
 		fig.suptitle(f'Rolling: {rolling}')
 
 		#ax.scatter(x, y, s=0.1)
-		axs[0].plot(x,y1,linewidth=.5, marker='o', markersize=2)
+		axs[0].plot(x,y1,linewidth=.5, markersize=2)
+		axs[0].scatter(x,y1,c=colors, s=2)
 		axs[0].set_ylabel('Total Reward')
 
-		axs[1].plot(x,y2,linewidth=.5, marker='o', markersize=2)
+		axs[1].plot(x,y2,linewidth=.5, markersize=2)
+		axs[1].scatter(x,y2,c=colors, s=2)
 		axs[1].set_xlabel('Epoch')
 		axs[1].set_ylabel('Avg Velocity')
 		plt.savefig(f'Plots/rewards_{rolling}')
