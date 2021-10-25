8c62434ae5f6fb9af566dd846cdbfb9d2032c7cd
MauriVass <maurizio140333@gmail.com>
2021-10-25 21:02:12 +0200

    Added rolling window

--- Content/MyContent/Server/confidence_intervals.py
+++ Content/MyContent/Server/confidence_intervals.py
@@ -24,13 +24,14 @@ def evaluate_conf_interval(data,confidence_level):
 	return avgs, lbs, ubs, ci
 
 confidence_level = 0.95
-n = 2
+n = 3
 model = 0
 runs_reward = []
 runs_speed = []
 for i in range(n):
 	data = pd.read_csv(f'Runs/Model{model}/{i}/run.rewards_{model}_{i}.csv')
-	runs_reward.append(data['totalReward'])
+	data['rolling_avg_reward'] = data['totalReward'].rolling(9).mean()
+	runs_reward.append(data['rolling_avg_reward'])#totalReward
 	runs_speed.append(data['averageSpeed'])
 avg_r, lbs_r, ubs_r, _ = evaluate_conf_interval(runs_reward,confidence_level)
 avg_s, lbs_s, ubs_s, _ = evaluate_conf_interval(runs_speed,confidence_level)
@@ -41,7 +42,10 @@ for i in range(len(avg_r)):
 	f.write(f'{avg_r[i]},{lbs_r[i]},{ubs_r[i]},{avg_s[i]},{lbs_s[i]},{ubs_s[i]}\n')
 f.close()
 
-plt.plot(range(len(avg_r)),avg_r, marker="o")
-plt.fill_between(range(len(avg_r)), lbs_r, ubs_r, alpha=0.2)
+fig, ax = plt.subplots(figsize=(12, 6))
+
+ax.plot(range(len(avg_r)),avg_r, markersize=1.5, marker="o")
+ax.fill_between(range(len(avg_r)), lbs_r, ubs_r, alpha=0.2)
+plt.ylim([0, 2000])
 plt.show()
 
